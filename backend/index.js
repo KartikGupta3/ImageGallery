998b8471ae8d1b285e6e6a70adccb3d26f864a2a
Kartik Gupta <kkartikgupta30@gmail.com>
2023-12-30 10:52:46 +0530

    login and signup routes

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,59 @@
 const express = require("express");
 import MongoConnect from "./config/Mdb";
-const app = express();
+const express = require("express");
+const cors = require("cors");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const cookieparser = require("cookie-parser");
+const UserModel = require("./models/User");
+
+app.use(express.json());
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    methods: ["GET,POST"],
+    credentials: true,
+  })
+);
+app.use(cookieparser());
 
 MongoConnect();
-app.listen(5000, () => {
+
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+  UserModel.findOne({ username: username }).then((users) => {
+    if (users) {
+      bcrypt.compare(password, users.password, (err, response) => {
+        if (response) {
+          const token = jwt.sign(
+            { username: users.username },
+            "jwt-secret-key",
+            { expiresIn: "1d" }
+          );
+          res.cookie("token", token);
+          return res.json("Success");
+        } else {
+          return res.json("Incorrect password");
+        }
+      });
+    } else {
+      res.json("Record didn't exist");
+    }
+  });
+});
+
+app.post("/register", (req, res) => {
+  const { username, password } = req.body;
+  bcrypt.hash(password, 10).then((hash) => {
+    UserModel.create({
+      username: username,
+      password: hash,
+    })
+      .then((user) => res.json({ status: "Success" }))
+      .catch((err) => res.json(err));
+  });
+});
+
+app.listen(4170, () => {
   console.log("Server is running");
 });
--- frontend/Components/HomePage.js
+++ frontend/Components/HomePage.js
@@ -12,7 +12,7 @@ const HomePage = () => {
     <>
       <div className="relative">
         <div className="flex items-center justify-center bg-gradient-to-r from-[#0F0C29] via-[#302B63] to-[#24243E] h-screen w-screen z-[9999] text-3xl text-white font-serif">
-          Welcome to Login/SignUp Portal
+          Welcome to Image Gallery
         </div>
       </div>
     </>
